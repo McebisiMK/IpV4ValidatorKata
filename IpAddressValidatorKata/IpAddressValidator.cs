6c179811804d3d747c338edaef156b2778cd5072
MCEBISI MKHOHLIWE <mcebisimkhohliwe@gmail.com>
2018-07-06 13:53:38 +0200

    Ip address V4 validation solution

---
+++ IpAddressValidatorKata/IpAddressValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace IpAddressValidatorKata
+{
+    public class IpAddressValidator
+    {
+        public bool ValidateIpv4Address(string ipAddress)
+        {
+            var blocks = ipAddress.Split('.').Length;
+            if (blocks == 4)
+            {
+                var status = ValidateIpAddress(ipAddress);
+
+                return status;
+            }
+            return false;
+        }
+
+        private bool ValidateIpAddress(string ipAddress)
+        {
+            var containsString = ipAddress.Split('.').Any(ip => int.TryParse(ip, out int invalid) == false);
+            if (containsString)
+            {
+                return false;
+            }
+            var lastBlock = ipAddress.Split('.').Last();
+            var incorrectLastBlock = new[] { "0", "255" };
+
+            return !incorrectLastBlock.Contains(lastBlock);
+        }
+
+    }
+}
---
+++ IpAddressValidatorKata/IpAddressValidatorTests.cs
@@ -0,0 +1,98 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IpAddressValidatorKata
+{
+    [TestFixture]
+    public class IpAddressValidatorTests
+    {
+        [TestCase("1.1.1.1")]
+        [TestCase("192.168.1.1")]
+        [TestCase("127.0.0.1")]
+        public void ValidateIpv4Address_GivenValidIpaddressWithFourBlocksSeparatedByDot_ShouldReturnTrue(string ipAddress)
+        {
+            //--------------------Arrange--------------------------------
+            var ipAddressValidator = CreateIpAddressValidator();
+
+            //--------------------Act------------------------------------
+            var actual = ipAddressValidator.ValidateIpv4Address(ipAddress);
+
+            //--------------------Assert---------------------------------
+            var expected = true;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1")]
+        [TestCase("192.168.5")]
+        [TestCase("10.0.0.1.4")]
+        public void ValidateIpv4Address_GivenIpaddressWithInvalidNumberOfBlocksSeparatedByDot_ShouldReturnTrue(string ipAddress)
+        {
+            //--------------------Arrange--------------------------------
+            var ipAddressValidator = CreateIpAddressValidator();
+
+            //--------------------Act------------------------------------
+            var actual = ipAddressValidator.ValidateIpv4Address(ipAddress);
+
+            //--------------------Assert---------------------------------
+            var expected = false;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1.1.1.0")]
+        [TestCase("192.168.1.0")]
+        [TestCase("10.0.0.0")]
+        public void ValidateIpv4Address_GivenIpaddressWithFourBlocksSeparatedByDotAndEndsWithZero_ShouldReturnFalse(string ipAddress)
+        {
+            //--------------------Arrange--------------------------------
+            var ipAddressValidator = CreateIpAddressValidator();
+
+            //--------------------Act------------------------------------
+            var actual = ipAddressValidator.ValidateIpv4Address(ipAddress);
+
+            //--------------------Assert---------------------------------
+            var expected = false;
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestCase("1.0.1.255")]
+        [TestCase("192.168.0.255")]
+        [TestCase("255.255.255.255")]
+        public void ValidateIpv4Address_GivenIpaddressWithFourBlocksSeparatedByDotAndEndsWith255_ShouldReturnFalse(string ipAddress)
+        {
+            //--------------------Arrange--------------------------------
+            var ipAddressValidator = CreateIpAddressValidator();
+
+            //--------------------Act------------------------------------
+            var actual = ipAddressValidator.ValidateIpv4Address(ipAddress);
+
+            //--------------------Assert---------------------------------
+            var expected = false;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("198.ten.1.1")]
+        [TestCase("198.ten.1.one")]
+        [TestCase("ten.one.one.1")]
+        public void ValidateIpv4Address_GivenIpaddressWithFourBlocksOneContainingString_ShouldReturnFalse(string ipAddress)
+        {
+            //--------------------Arrange--------------------------------
+            var ipAddressValidator = CreateIpAddressValidator();
+
+            //--------------------Act------------------------------------
+            var actual = ipAddressValidator.ValidateIpv4Address(ipAddress);
+
+            //--------------------Assert---------------------------------
+            var expected = false;
+            Assert.AreEqual(expected, actual);
+        }
+
+        private static IpAddressValidator CreateIpAddressValidator()
+        {
+            return new IpAddressValidator();
+        }
+    }
+}
